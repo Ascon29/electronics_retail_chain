ea40f2bd15b0eafd75a55ac817352d6abfdf4b40
Виктор Бугай <ascon2913@gmail.com>
2025-03-20 21:46:53 +0500

    develop branch

--- business/admin.py
+++ business/admin.py
@@ -12,10 +12,12 @@ class CompanyAdmin(admin.ModelAdmin):
 
     @admin.display(description="Ссылка на поставщика")
     def supplier_link(self, obj):
+        """ Добавление ссылки на поставщика """
         if obj.supplier:
             return format_html("<a href='{url}'>{url_name}</a>", url=obj.supplier.id, url_name=obj.supplier)
 
     @admin.action(description="Очистить задолженность перед поставщиком")
     def clear_debt(self, request, queryset):
+        """ Admin action, очищающий задолженность перед поставщиком у выбранных объектов. """
         queryset.update(debt=0)
         self.message_user(request, "Задолженность перед поставщиком очищена")
